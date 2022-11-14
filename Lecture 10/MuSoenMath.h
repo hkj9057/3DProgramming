aa8883183000cfc66d0a1765a1df4db278bc3aa7
hkj9057@office.yuhan.ac.kr <hkj9057@office.yuhan.ac.kr>
2022-11-14 23:16:19 +0900

    [Lecture 11] 3D프로그래밍 11주차 과제... 교수님 바다불

--- Lecture 10/MuSoenMath.h
+++ Lecture 10/MuSoenMath.h
@@ -1,33 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
-class vec3 {      // vec3 클래스 만들기
+class vec3 {                                        // vec3 클래스 만들기
 public:
-    float v[3][1] = { {2}, {2}, {1} };         // Vector값을 배열로 정렬 (x, y, z)
+    float v[3][1] = { {2}, {2}, {1} };             // Vector값을 배열로 정렬 (x, y, z)
     float v_col = sizeof(v[0]) / sizeof(float);   // 행벡터값의 크기
-    float v_row = sizeof(v) / sizeof(v[0]);      // 열벡터값의 크기
-    float p[3][3] = { 0 };                  // 최종 결과값 저장용
+    float v_row = sizeof(v) / sizeof(v[0]);       // 열벡터값의 크기
+    float p[3][3] = { 0 };                       // 최종 결과값 저장용
 private:
 };
 
-class mat3 {      // mat3 클래스 만들기, TSR정의 및 데이터 삽입
+class mat3 {                                    // mat3 클래스 만들기, TSR정의 및 데이터 삽입
 public:
     float Tr[3][3] = {
-                   {1, 0, 1},      // 3*3 Translate 정의 및 데이터 삽입
+                   {1, 0, 1},                   // 3*3 Translate 정의 및 데이터 삽입
                    {0, 1, 0},
                    {3, 5, 1}
     };
     float Sc[3][3] = {
-                   {2, 0, 0},      // 3*3 Scale 정의 및 데이터 삽입
+                   {2, 0, 0},                   // 3*3 Scale 정의 및 데이터 삽입
                    {0, 2, 0},
                    {0, 0, 1}
     };
-    float Pi = 3.14;               // 원주율 선언
-    float cos30 = 0.15;               // cos 30값 선언
-    float sin30 = 0.5;               // sin 30값 선언
+    float Pi = 3.14;                            // 원주율 선언
+    float cos30 = 0.15;                          // cos 30값 선언
+    float sin30 = 0.5;                          // sin 30값 선언
 
     float Rt[3][3] = {
-                   { cos30, sin30, 0 },   // 3*3 Rotate 정의 및 데이터 삽입
+                   { cos30, sin30, 0 },         // 3*3 Rotate 정의 및 데이터 삽입
                    { -sin30, cos30, 0 },
                    {     0,      0, 1 }
     };
@@ -36,17 +37,17 @@ private:
 
 void header()
 {
-    float a[3][3] = { 0 };            // 추후 행렬곱 데이터를 삽입하기 위한 3*3 행렬
-    float b[3][3] = { 0 };            // 추후 행렬곱 데이터를 삽입하기 위한 3*3 행렬;
+    float a[3][3] = { 0 };                      // 추후 행렬곱 데이터를 삽입하기 위한 3*3 행렬
+    float b[3][3] = { 0 };                      // 추후 행렬곱 데이터를 삽입하기 위한 3*3 행렬;
 
-    mat3 Translate, Rotate, Scale;      // mat3 안의 TSR를 가져오기 위한 변수
-    vec3 Vector, P;                  // vec3 안의 Vector, P를 가져오기 위한 변수
+    mat3 Translate, Rotate, Scale;              // mat3 안의 TSR를 가져오기 위한 변수
+    vec3 Vector, P;                             // vec3 안의 Vector, P를 가져오기 위한 변수
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 3; k++) {
-                a[i][j] += Scale.Sc[i][k] * Translate.Tr[k][j];
-                // Scale 행렬 *  Translate 행렬 = 행렬 a 삽입
+                a[i][j] += Scale.Sc[i][k] * 
+                    Translate.Tr[k][j];              // Scale 행렬 *  Translate 행렬 = 행렬 a 삽입
             }
         }
     }
@@ -54,8 +55,9 @@ void header()
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 3; k++) {
-                b[i][j] += Rotate.Rt[i][k] * a[k][j];
-                // Rotate 행렬 * 위의 행렬 a(Sc * Tr) = 행렬 b 삽입
+                b[i][j] += 
+                    Rotate.Rt[i][k] * a[k][j];
+                                                         // Rotate 행렬 * 위의 행렬 a(Sc * Tr) = 행렬 b 삽입
             }
         }
     }
@@ -63,8 +65,9 @@ void header()
     for (int i = 0; i < Vector.v_row; i++) {
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < Vector.v_col; k++) {
-                P.p[i][j] += Vector.v[i][k] * b[k][j];
-                // Vector 행렬 * 위의 행렬 b(Tr * Sc * Rt) = vec3 p 행렬 삽입
+                P.p[i][j] 
+                    += Vector.v[i][k] * b[k][j];
+                                                         // Vector 행렬 * 위의 행렬 b(Tr * Sc * Rt) = vec3 p 행렬 삽입
             }
         }
     }
---
+++ Lecture 11/Lecture 11.cpp
@@ -0,0 +1,27 @@
+﻿#include "MuSoenMath.h"
+#include <iostream>
+//#include"multiplymatrix.h"
+//#include"multiplytransposematrix.h"
+
+int main() {
+    //multiplyTransposeMatrix();
+    //IdentityMatrix();
+    //TransposeMatrix();
+    //multiplyMatrix();
+    multiplyTransposeMatrix();
+    //Vec3 Vector(0, 0); //원점을 0,0으로 설정
+    //Mat3 Matrix;//행렬 생성
+
+    //Matrix.Translate(3, 5); // 3,5로 이동
+    //Vector = Vector * Matrix; // 연산자 오버로딩으로 백터와 행렬 계산
+    //Matrix.Rotate(30); // 30도 회전
+    //Vector = Vector * Matrix;// 연산자 오버로딩으로 백터와 행렬 계산
+    //Matrix.Scale(2); // 크기를 2배로 확대
+    //Vector = Vector * Matrix;// 연산자 오버로딩으로 백터와 행렬 계산
+    // 
+    //cout << fixed;
+    //cout.precision(0); // 소수점 제거
+
+    //cout << "원점이 0,0일때 3,5이동 30도회전 2배확대했을때의 좌표값은: " << endl;
+    //cout << Vector.Position[0] << "," << Vector.Position[1] << "입니다." << endl;
+}
